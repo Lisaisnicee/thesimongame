f189244dd1bb033072023c8a88bc210d59a8e1c3
lisa <lisa.mazno@gmail.com>
2023-02-24 09:20:22 +0100

    2nd  commit

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 class Game {
   constructor() {
     this.colors = ["red", "green", "blue", "orange"];
-    this.pattern = [];
-    this.playerPattern = [];
+    this.sequence = [];
+    this.playerSequence = [];
     this.round = 0;
   }
 
@@ -13,28 +13,34 @@ class Game {
 
   play() {
     this.round = 0;
-    this.pattern = [];
-    this.playerPattern = [];
+    this.sequence = [];
+    this.playerSequence = [];
     this.display.showMessage('À ton tour !');
     setTimeout(() => {
+      //L'IA ajoute une couleur et l'anime sur l'écran
       this.addColor();
-      this.display.flashColors(this.pattern, () => {
+      this.display.flashColors(this.sequence, () => {
+        //On attend que le joueur entre sa séquence de couleurs
         this.waitForPlayer();
       });
     }, 1500);
   }
   
   waitForPlayer() {
-    if (this.playerPattern.length === this.pattern.length) {
-      if (this.playerPattern.every((val, i) => val === this.pattern[i])) {
-       
+    //On compare sa séquence à celle de l'IA
+    if (this.playerSequence.length === this.sequence.length) {
+      //every pour comparer le contenu des deux sequences
+      if (this.playerSequence.every((val, i) => val === this.sequence[i])) {
+       //incrementation du round et remise à zéro de la sequence du joueur
         this.round++;
-        this.playerPattern = [];
+        this.playerSequence = [];
+        //nombre de round max à 5
         if (this.round < 5) {
           this.display.showMessage(`Bravo ! Niveau ${this.round} réussi !`);
           setTimeout(() => {
+            //la boucle se repéte tant que le joueur n'a pas entré une mauvaise couleur
             this.addColor();
-            this.display.flashColors(this.pattern, () => {
+            this.display.flashColors(this.sequence, () => {
               this.waitForPlayer();
             });
           }, 1500);
@@ -45,33 +51,40 @@ class Game {
 
         this.display.showMessage(`Raté! Tu as perdu au niveau ${this.round}.`);
       }
-    } else {
+    } 
+    
+    else {
      
       const squares = document.querySelectorAll('.square');
       squares.forEach((square) => {
         square.addEventListener('click', () => {
+
+
           const selectedColor = square.id;
-          this.playerPattern.push(selectedColor);
+          this.playerSequence.push(selectedColor);
           this.display.highlightColor(selectedColor);
           setTimeout(() => {
             this.display.clearHighlight(selectedColor);
           }, 500);
           this.waitForPlayer();
+
+          
         });
       });
     }
   }
   
-
+//méthode qui permet à l'IA d'ajouter une couleur aléatoire à sa séquence
   addColor() {
     const nextColor =
       this.colors[Math.floor(Math.random() * this.colors.length)];
  
-    this.pattern.push(nextColor);
-    console.log("ia adding a color: " + this.pattern);
+    this.sequence.push(nextColor);
+    console.log("ia adding a color: " + this.sequence);
   }
+
   checkTap(color) {
-    this.playerPattern.push(color);
+    this.playerSequence.push(color);
     this.waitForPlayer();
   }
 }
